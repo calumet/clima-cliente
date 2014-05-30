e52fc6063382a02965fc170aef0f8082fd34b9c5
Romel Pérez <ronelprhone@gmail.com>
2014-05-30 00:00:18 -0500

    Initial structure

---
+++ src/weather/Weather.java
@@ -0,0 +1,17 @@
+/*!
+ * Grupo de Desarrollo de Software Calumet
+ * Weather | Application | Main Class
+ * Romel Pérez @prhonedev, 2014
+ */
+
+package weather;
+
+public class Weather {
+
+    public static void main(String[] args) {
+        
+        // TODO
+        
+    }
+    
+}
